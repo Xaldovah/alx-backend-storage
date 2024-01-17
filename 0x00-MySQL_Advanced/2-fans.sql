b6ce7b0e43936de3012791b03fd2722edca73635
xaldovah <dxaldovah@gmail.com>
2024-01-18 02:14:10 +0300

    SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- Import the metal_bands table dump
+-- Rank country origins of bands by the number of (non-unique) fans
+SELECT origin, COUNT(*) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;

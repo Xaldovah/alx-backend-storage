5551e9f7f6e12c9a08650ede7c8ef5e85bd2d9a0
xaldovah <dxaldovah@gmail.com>
2024-01-18 15:34:34 +0300

    SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0

--- 0x00-MySQL_Advanced/7-main.sql
+++ 0x00-MySQL_Advanced/7-main.sql
@@ -4,6 +4,7 @@ SELECT * FROM corrections;
 
 SELECT "--";
 CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Bob"));
 
 SELECT "--";
 SELECT * FROM users;
